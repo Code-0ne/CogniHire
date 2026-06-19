231a383c383f9a51d4668ddebc22e6f686ea21a5
GhostisLive <rwitabratohanpal@gmail.com>
2026-06-19 21:46:01 +0530

    minor changes

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from config import *
+from loader import *
+from jd_parser import get_jd_cont
+from text_builder import build_rich_txt, scan_prod, scan_research
+from precompute import setup_artifacts, generate_candidate_embeddings, build_and_save_faiss_index 
